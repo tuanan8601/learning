2c4b0b65f8f9c933f5f5b5961a9402f124a15004
admin <tuanan8601@gmail.com>
2021-09-06 12:06:44 +0700

    Learning

--- src/main/java/api/WelcomeResource.java
+++ src/main/java/api/WelcomeResource.java
@@ -7,7 +7,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Path("/")
 public class WelcomeResource {
@@ -18,7 +20,11 @@ public class WelcomeResource {
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public List<Subject> welcome(){
-        return subjectService.getAllSubject();
+    public Map<String,String> welcome(){
+        Map<String,String> subject_map = new HashMap<>();
+        subjectService.getAllSubject().forEach(d->{
+            subject_map.put(d.getName(),d.getSubjectId());
+        });
+        return subject_map;
     }
 }

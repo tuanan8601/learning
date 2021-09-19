06be808c198ccfc1ea75e66a65b1b4a7a10481a5
admin <tuanan8601@gmail.com>
2021-09-19 09:27:39 +0700

    Learning

--- src/main/java/DAO/RedisDB/AbsDAO.java
+++ src/main/java/DAO/RedisDB/AbsDAO.java
@@ -5,8 +5,9 @@ import redis.clients.jedis.Jedis;
 public abstract class AbsDAO {
     static Jedis jedis;
     Jedis getdb(){
-        jedis = new Jedis("localhost");
-        System.out.println("Connection to server sucessfully");
+        Jedis jedis = new Jedis("redis-18337.c233.eu-west-1-1.ec2.cloud.redislabs.com", 18337);
+        jedis.auth("fourin1234");
+        System.out.println("Connected to Redis");
         return jedis;
     }
 }

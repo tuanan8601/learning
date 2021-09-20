d0d50ebef78c6f6d4488620b85acb4bff9549178
admin <tuanan8601@gmail.com>
2021-09-20 16:32:49 +0700

    Learning

--- src/main/java/DAO/RedisDB/AbsDAO.java
+++ src/main/java/DAO/RedisDB/AbsDAO.java
@@ -1,10 +1,17 @@
 package DAO.RedisDB;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.exceptions.JedisException;
 
 public abstract class AbsDAO {
-    Jedis getConnection(){
-        Jedis jedis = new Jedis("localhost");
+    Jedis getConnection() throws JedisException {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        JedisPool pool = new JedisPool(jedisPoolConfig,"redis-18337.c233.eu-west-1-1.ec2.cloud.redislabs.com",
+                18337,100,"fourin1234");
+        Jedis jedis = pool.getResource();
+
 //        Jedis jedis = new Jedis("redis-18337.c233.eu-west-1-1.ec2.cloud.redislabs.com", 18337);
 //        jedis.auth("fourin1234");
         System.out.println("Connected to Redis");

f9915072321f0239da2860a73e2820ff9f83891a
admin <tuanan8601@gmail.com>
2021-09-20 20:38:49 +0700

    Learning

--- src/main/java/DAO/RedisDB/AbsDAO.java
+++ src/main/java/DAO/RedisDB/AbsDAO.java
@@ -13,7 +13,7 @@ public abstract class AbsDAO {
     Jedis getConnection() throws JedisException {
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
         JedisPool pool = new JedisPool(jedisPoolConfig,"redis-18337.c233.eu-west-1-1.ec2.cloud.redislabs.com",
-                18337,60,"fourin1234");
+                18337,300,"fourin1234");
         Jedis jedis = pool.getResource();
 //        Jedis jedis = new Jedis("redis-18337.c233.eu-west-1-1.ec2.cloud.redislabs.com", 18337);
 //        jedis.auth("fourin1234");

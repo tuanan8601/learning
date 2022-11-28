ec0f9ecb1cccfc9d3c642a04f0197220037eaee6
admin <tuanan8601@gmail.com>
2022-11-28 13:54:34 +0700

    an

--- src/main/java/DAO/MongoDB/AbsDAO.java
+++ src/main/java/DAO/MongoDB/AbsDAO.java
@@ -19,7 +19,8 @@ public abstract class AbsDAO {
 
     MongoDatabase getDB() {
         if (db == null) {
-            ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
+//            ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017");
+            ConnectionString connectionString = new ConnectionString("mongodb+srv://tuanan8601:fourin1234@cluster0.u4rda.mongodb.net/?retryWrites=true&w=majority");
             MongoClientSettings settings = MongoClientSettings.builder()
                     .applyConnectionString(connectionString)
                     .codecRegistry(fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build())))
